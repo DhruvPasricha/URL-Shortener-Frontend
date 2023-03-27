74fb1a39535bb938ed97a52fba77cb32a88f2d0d
Dhruv Pasricha <dhruvpasricha0002@gmail.com>
2023-03-27 14:39:16 +0530

    add frontend

---
+++ src/Components/Redirection/Redirect.tsx
@@ -0,0 +1,28 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+export const Redirect = () => {
+    const [longURL, setLongURL] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const redirect = async () => {
+        try {
+            setIsLoading(true);
+            const shortURL = window.location.pathname;
+            const res = await axios.get(`http://localhost:5000${shortURL}`);
+            setLongURL(res.data.longURL);
+        } catch {
+            setIsLoading(false);
+        }
+    };
+    useEffect(() => {
+        if (longURL) {
+            window.location.href = longURL;
+        } else {
+            redirect();
+        }
+    }, [longURL]);
+    if (isLoading) {
+        return <>Loading......</>;
+    }
+    return <>No URL Matched</>;
+};
---
+++ src/Pages/Home/Home.tsx
@@ -0,0 +1,56 @@
+import axios from "axios";
+import React, { useState } from "react";
+import "./Home.css";
+
+export default function Home() {
+    const [longURL, setLongURL] = useState("");
+    const [shortURL, setShortURL] = useState("");
+
+    const getShortURL = async (event: { preventDefault: () => void }) => {
+        event.preventDefault();
+        try {
+            const res = await axios.post("http://localhost:5000/api/shorten", {
+                longURL,
+            });
+            setShortURL(res.data.shortURL);
+        } catch (error) {
+            setShortURL('');
+            alert("Invalid URL");
+            console.log(error);
+        }
+    };
+
+    return (
+        <>
+            <h1>URL Shortener</h1>
+            <form>
+                <input
+                    type="text"
+                    placeholder="Enter URL"
+                    required
+                    onChange={(event) => {
+                        setLongURL(event.target.value);
+                    }}
+                    style={{
+                        width: "200px",
+                        marginRight: "5px",
+                    }}
+                ></input>
+                <button type="submit" onClick={getShortURL}>
+                    Shorten URL
+                </button>
+            </form>
+            {shortURL && (
+                <div className="shortUrlContainer">
+                    Short URL :{" "}
+                    <a
+                        href={`/${shortURL}`}
+                        target="_blank"
+                    >
+                        {shortURL}
+                    </a>
+                </div>
+            )}
+        </>
+    );
+}
